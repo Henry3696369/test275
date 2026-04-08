2ee0ea39d57051081ccd358187cad3836cf0a512
Heng Luo <luoheng36963@gmail.com>
2026-04-08 18:25:16 -0400

    Change Router to HashRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import type { Project } from "./Interface/Project";
 import { PROJECTS } from "./Interface/Project";
 
